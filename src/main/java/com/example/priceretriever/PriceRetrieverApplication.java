a14391918145f3563eda91cbb5f458056b5d1040
Armando Rivas Arzaluz <armandorivasarzaluz@Armandos-MacBook-Pro.local>
2022-07-11 10:17:18 -0500

    Added initial project

---
+++ src/main/java/com/example/priceretriever/PriceRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.example.priceretriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceRetrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceRetrieverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/priceretriever/PriceRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.priceretriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceRetrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
